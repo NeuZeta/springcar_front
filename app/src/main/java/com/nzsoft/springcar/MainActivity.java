6e8c63bcd0af453c5cd09c2d16b89d8a7119f919
NeuZeta <neuzeta@gmail.com>
2019-07-22 14:20:44 +0200

    tests with expandablelistview

--- app/src/main/java/com/nzsoft/springcar/MainActivity.java
+++ app/src/main/java/com/nzsoft/springcar/MainActivity.java
@@ -1,11 +1,16 @@
 package com.nzsoft.springcar;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.widget.ExpandableListView;
 
 
 import com.nzsoft.springcar.adapters.ExpandableListAdapter;
+import com.nzsoft.springcar.adapters.testAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ExpandableListView listView;
     private ExpandableListAdapter adapter;
+    private testAdapter adapterTest;
     private List<String> listDataHeaders;
+    private List<Fragment> fragments;
     private Map<String, List<String>> listHashmap;
 
     @Override
@@ -27,28 +34,32 @@ public class MainActivity extends AppCompatActivity {
         initData();
 
         listView = (ExpandableListView) findViewById(R.id.levelExp);
-        adapter = new ExpandableListAdapter(this, listDataHeaders, listHashmap);
-        listView.setAdapter(adapter);
+        //adapter = new ExpandableListAdapter(this, listDataHeaders, listHashmap);
+        //listView.setAdapter(adapter);
+
+        adapterTest = new testAdapter(this, listDataHeaders, fragments);
+        listView.setAdapter(adapterTest);
+
     }
 
     private void initData() {
 
         listDataHeaders = new ArrayList<>();
-        listDataHeaders.add("Opcion 1");
-        listDataHeaders.add("Opcion 2");
-        listDataHeaders.add("Opcion 3");
+        listDataHeaders.add("Dates Selection");
+        listDataHeaders.add("Car Selection");
+        listDataHeaders.add("Extras Selection");
 
         List<String> opciones1 = new ArrayList<>();
-        opciones1.add("SubOpcion 1");
+        opciones1.add("SubOpcion 1.1");
 
         List<String> opciones2 = new ArrayList<>();
-        opciones2.add("SubOpcion 1");
-        opciones2.add("SubOpcion 2");
-        opciones2.add("SubOpcion 3");
+        opciones2.add("SubOpcion 2.1");
+        opciones2.add("SubOpcion 2.2");
+        opciones2.add("SubOpcion 2.3");
 
         List<String> opciones3 = new ArrayList<>();
-        opciones3.add("SubOpcion 1");
-        opciones3.add("SubOpcion 2");
+        opciones3.add("SubOpcion 3.1");
+        opciones3.add("SubOpcion 3.2");
 
 
         listHashmap = new HashMap<>();
@@ -56,6 +67,9 @@ public class MainActivity extends AppCompatActivity {
         listHashmap.put(listDataHeaders.get(1), opciones2);
         listHashmap.put(listDataHeaders.get(2), opciones3);
 
-
+        fragments = new ArrayList<>();
+        fragments.add(new BlankFragment());
+        fragments.add(new BlankFragment2());
+        fragments.add(new BlankFragment2());
     }
 }
